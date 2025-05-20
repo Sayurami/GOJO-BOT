6d2584e5abac1c0559dcf37ac71b8ac07a2be783
Gojo899 <gojosathory@gmail.com>
2025-05-20 11:22:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "SZsmjAaL#5po9EAETJCYT2i8Z0VJ9lh9nvc0vI6oe4pI__Ts6DNc",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:KjXCTHcONiyEMVZnRMxadRtvtxsnhHnt@ballast.proxy.rlwy.net:51017",
   OWNER_NUM: process.env.OWNER_NUM || "94743826406",
 };
