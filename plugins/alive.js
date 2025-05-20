a0b92dee6d1fa4cb61ddad881a56e164327c592e
Gojo899 <gojosathory@gmail.com>
2025-05-20 09:07:08 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,39 +1,22 @@
-const { cmd, commands } = require('../command');
-const config = require('../config');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
-    alias: ["bot", "robot"],
-    react: "👻",
-    desc: "Bot එක ඔන්ලයින්ද බලන්න.",
+    alias: "bot",
+    react:"👻",
+    desc: "Check bot online or no.",
     category: "main",
     filename: __filename
 },
-async (robin, mek, m, {
-    from, quoted, body, isCmd, command, args, q, isGroup,
-    sender, senderNumber, botNumber2, botNumber, pushname,
-    isMe, isOwner, groupMetadata, groupName, participants,
-    groupAdmins, isBotAdmins, isAdmins, reply
-}) => {
-    try {
-        const ALIVE_IMG = process.env.ALIVE_IMG || "https://example.com/default.jpg";
-        const ALIVE_MSG = process.env.ALIVE_MSG || "Hey! Bot එක ඔන්ලයින් එකයි!";
+async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const config = await readEnv();
+return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
 
-        await robin.sendMessage(from, {
-            image: { url: ALIVE_IMG },
-            caption: ALIVE_MSG
-        }, { quoted: mek });
-
-        await robin.sendMessage(from, {
-            audio: {
-                url: "https://github.com/Gojo899/Bot-photo-and-video-/raw/refs/heads/main/mp3/1747630063493150813rf60n6vl-voicemaker.in-speech.mp3"
-            },
-            mimetype: 'audio/mpeg',
-            ptt: true
-        }, { quoted: mek });
-
-    } catch (e) {
-        console.log(e);
-        reply(`${e}`);
-    }
-});
